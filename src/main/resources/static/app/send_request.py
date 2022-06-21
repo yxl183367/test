86acf650abad29b72e83cc9f386a60c6255e04c8
yxl183367 <yxl183367@163.com>
2022-06-21 09:34:42 +0800

    测试

--- src/main/resources/static/app/send_request.py
+++ src/main/resources/static/app/send_request.py
@@ -3,25 +3,22 @@ import requests
 import json
 
 def test():
+    url = 'http://localhost:8080/test'
+    params = {
+        "name": "小明"
+    }
+
+    rep = requests.post(url=url, data=params)
+    # 返回字符串数据
+    sul = rep.json()
     try:
-        url = 'http://localhost:8080/test'
-        params = {
-            "name":"小明"
-        }
-
-        rep = requests.post(url=url,data=params)
-        # 返回字符串数据
-        sul = rep.json()
-
-        print(sul["code"])
-
         if sul["code"] == "200":
             print("返回状态码异常")
-            return sul["msg"]
+            raise Exception()
 
         return "执行成功"
     except:
-        print("失败")
+        print(sul["data"])
 
 
 
