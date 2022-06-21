ac770bb7e6e48874154875389b861e8071af7088
yxl183367 <yxl183367@163.com>
2022-06-21 09:24:17 +0800

    测试

--- src/main/java/com/example/demo/test/TestController.java
+++ src/main/java/com/example/demo/test/TestController.java
@@ -1,33 +1,34 @@
 package com.example.demo.test;
 
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
 
 
     @RequestMapping("/test")
-    public String test(@RequestParam String name) throws Exception {
+    public Map<Object, Object> test(@RequestParam String name) throws Exception {
 
+        Map<Object, Object> map = new HashMap<>();
         try {
+            map.put("code","200");
+            map.put("msg","成功");
             if ("小明".equals(name)) {
-                return name;
-            }
-            String s = null;
-            if(s.equals("aaa")){
-                System.out.println("111111111111");
+                map.put("data",name);
+                return map;
             }
 
         } catch (Exception e) {
             throw new Exception(e.toString());
         }
-        return "Hello word!!!";
+        map.put("data","not find");
+        return map;
 
     }
 }
--- src/main/resources/static/app/send_request.py
+++ src/main/resources/static/app/send_request.py
@@ -1,19 +1,25 @@
 
 import requests
-
+import json
 
 def test():
     try:
-    url = 'http://localhost:8080/test'
+        url = 'http://localhost:8080/test'
         params = {
             "name":"小明"
         }
 
         rep = requests.post(url=url,data=params)
         # 返回字符串数据
-        print(rep.text)
-        return rep.text
+        sul = rep.json()
 
+        print(sul["code"])
+
+        if sul["code"] == "200":
+            print("返回状态码异常")
+            return sul["msg"]
+
+        return "执行成功"
     except:
         print("失败")
 
